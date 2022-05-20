ff8a66ea0538cde4d3c6a9bf328734df6ea0170c
Jaber Elferkh <elferkhjaber@gmail.com>
2022-05-20 13:29:21 +0100

    feat: Start on Main section

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
-    <div className='w-screen h-screen'>
+    <div className='w-screen h-screen overflow-x-hidden'>
       <Head>
         <title>Suzano Café</title>
         <link rel='icon' href='/images/Suzano-favicon.png' />
@@ -27,7 +27,41 @@ const Home: NextPage = () => {
 
         <Header />
 
-        {/* <h1 className='text-4xl'>Suzano Café</h1> */}
+        <div className='border-red flex items-center h-[calc(100%-150px)] overflow-hidden'>
+          <div className='flex-1 border-red h-full relative'>
+            <div className='mt-[56px]'>
+              <span className='text-[#C6453E] text-base font-normal mb-3 inline-block'>
+                Coffee Time.... ☕
+              </span>
+              <h1 className='text-[56px] font-Romelio font-100 leading-[72px] text-[#5E1F12]'>
+                Enjoy Your <br /> Morning <span className='text-[#C6453E]'>Coffee.</span>
+              </h1>
+            </div>
+
+            <div className='absolute left-[30px] bottom-[-80px] scale-125'>
+              <Image
+                src={'/images/splash-1.png'}
+                width={200}
+                height={200}
+                objectFit='contain'
+                alt='splash-1'
+              />
+            </div>
+          </div>
+
+          <div className='relative flex-1 h-full'>
+            <div className='absolute right-[-180px] top-[30%]'>
+              <div className='w-[878px] h-[956px] relative'>
+                <Image
+                  src={'/images/Bg-gradient.png'}
+                  layout='fill'
+                  objectFit='contain'
+                  alt='bg-gradient'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Romelio: ['Romelio', ...defaultTheme.fontFamily.sans],
+      },
+      fontWeight: {
+        100: 100,
+      },
+    },
   },
   plugins: [],
 };
